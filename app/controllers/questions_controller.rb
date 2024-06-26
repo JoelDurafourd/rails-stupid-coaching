890a8e96ac907ef5d94053acb6a9e4ddf8f8e7ab
Joel Durafourd <joel.durafourd@gmail.com>
2024-06-26 20:33:06 +0900

    finished this challenge

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,16 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
+  def answer
+    @question = params[:question]
+    @coach_reply = ""
+    if @question.downcase == "i am going to work"
+      @coach_reply = "Great!"
+    elsif @question.end_with?("?")
+      @coach_reply = "Silly question, get dressed and go to work!"
+    else
+      @coach_reply = "I don't care, get dressed and go to work!"
+    end
+  end
 end
